7741706ac76c34052dd09264cac69b49533695f8
Ilya Andreev <Franklylakers@yandex.ru>
2022-11-26 22:59:37 +0700

    add Example 10

---
+++ Example10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter a three-digit number: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num > 99 & num <= 999)
+{
+    Console.WriteLine(num / 10 % 10);
+}
+else
+{
+    Console.WriteLine("invalid value");
+}
