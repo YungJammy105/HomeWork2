f31230def7164a4586e0cceaa39fddaadaa66c39
Ilya Andreev <Franklylakers@yandex.ru>
2022-11-27 22:00:55 +0700

    add Example13

---
+++ Example13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 99)
+{
+    Console.WriteLine("No third digit. Enter another number");
+}
+else
+{
+    while(num > 1000)
+    {
+       num = num / 10;
+    }
+    num = num % 10;
+    Console.WriteLine(num);
+}
