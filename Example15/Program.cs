9caa45112f5316bf2611063d2223bcfbef0bb325
Ilya Andreev <Franklylakers@yandex.ru>
2022-11-27 22:19:34 +0700

    add Example15

---
+++ Example15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter number: ");
+int day = int.Parse(Console.ReadLine());
+
+if(day >= 1 && day <= 5)
+{
+    Console.WriteLine("This is weekday");
+}
+else if(day == 6 | day == 7)
+{
+    Console.WriteLine("This is weekend");
+}
+else
+{
+    Console.WriteLine("Unvaild value");
+}
